b498a4db113ce4dc361bde3fa5f0c7a7b4040e85
Oscar Sirvent <oscar.sirvent@gmail.com>
2021-02-02 09:49:12 +0100

    variants Button Test

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import * as React from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
-  theme,
+  Button
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import theme from "./theme/index"
+//import theme from "./theme"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -18,19 +16,15 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <Button>
+            baseStyle
+          </Button>
+          <Button variant="">
+            variant=""
+          </Button>
+          <Button variant="blue">
+            variant="blue"
+          </Button>
         </VStack>
       </Grid>
     </Box>
